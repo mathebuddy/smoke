ad4ceb0dc168c5fdde1b2b9fab3fc95da8234e21
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-07-03 09:52:58 +0200

    updates

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "f6b1e481994b799ace9d51b8fe335e52",
-"index.html": "d00ae20ecbd92f0dc0176b0da589ced8",
-"/": "d00ae20ecbd92f0dc0176b0da589ced8",
+"index.html": "c686dac7e2d3f71ba93551971a285df2",
+"/": "c686dac7e2d3f71ba93551971a285df2",
 "main.dart.js": "59c1a571d57140881eb4efcf4c1504b4",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "85964310ed8019418c7134094b6e63ac",
@@ -28,7 +28,7 @@ const RESOURCES = {
 "assets/assets/img/logo-large-en.png": "5b4cba2e3ba850ec9039165ac7ba7e14",
 "assets/assets/img/logo-large-de.png": "eb1ba8c2c4d04ea072c4c8c2239bd950",
 "assets/assets/img/logo.png": "5244282b4556da033d86dd0c8d14bd49",
-"assets/assets/bundle-complex.json": "c3ca3e037294122c0161978108ef002f",
+"assets/assets/bundle-complex.json": "7ae75cfc241af2aaab734af5356574e9",
 "canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
 "canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
 "canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
